06a5a80a37855e16a5ff69ec78696d485e30e63a
kencat0314 <chiaowei.wu@stud.h-da.de>
2025-10-12 14:35:01 +0200

    create exe

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.json());
 app.use(express.json({
   limit: '1mb',
-  verify: (req, res, buf) => { req.rawBody = buf; },
+  verify: (req, res, buf) => { req.rawBody = buf.toString(); },
 }));
 
 app.use(express.urlencoded({ extended: true }));
